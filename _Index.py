634c63b2646f315a86fe48758b85ec7d84f8e0a1
Leandro Rodrigues <leandromartinsr10@hotmail.com>
2025-10-03 07:39:50 -0300

    .

--- _Index.py
+++ _Index.py
@@ -1,7 +1,7 @@
 import time, json
 from lnmarkets import rest
 from decouple import config
-from _Telegram_Mensagem import enviarMensagem
+from _telegram import enviarMensagem
 
 options = {"key": config("LNM_KEY"), "secret": config("LNM_SECRET"), "passphrase": config("LNM_PASSPHRASE"), "network": config("LNM_NETWORK")}
 lnm = rest.LNMarketsRest(**options)
@@ -10,26 +10,23 @@ print("Olá Mestre! Iniciando...")
 print("Testando Telegram...")
 enviarMensagem("Testando Telegram...")
 
-ultimo_preco = 115_900
+ultimo_preco = 120_000
 
 while True:
     try:
-        ####################### ARMAZENAR ORDENS ABERTAS E MONTANTE DISPONIVEL #########################
-
+        # ARMAZENAR ORDENS ABERTAS E MONTANTE DISPONIVEL
         user_data = json.loads(lnm.get_user())
         trades_abertos = json.loads(lnm.futures_get_trades({"type": "running"}))
 
-        ################################ PREÇO ATUAL DO BITCOIN ################################
-
+        # PREÇO ATUAL DO BITCOIN
         start = time.time()
         ticker = json.loads(lnm.futures_get_ticker())
         preco_atual = ticker["lastPrice"]
         print(f"Preço atual: {preco_atual} | Tempo: {time.time() - start:.2f}")
 
-        ################################ ENVIANDO ORDEM DE COMPRA ################################
-
+        # ENVIANDO ORDEM DE COMPRA
         preco_limite, variacao = 250_000, 0.007
-        quantity, leverage = 123, 10
+        quantity, leverage = 280, 10
 
         if user_data["balance"] > preco_limite and abs(preco_atual - ultimo_preco) >= ultimo_preco * variacao:
 
@@ -39,8 +36,7 @@ while True:
             enviarMensagem(f"Ordem Enviada: Compra Efetivada ✅\nPreço De Compra: {new_trade['price']} 🎯")
             ultimo_preco = new_trade["price"]
 
-        ################################ FECHANDO ORDENS ABERTAS ################################
-
+        # FECHANDO ORDENS ABERTAS
         for ordem in trades_abertos:
             lucro, taxa = 0.005, 0.002
             taxa_funding = ((ordem["sum_carry_fees"] / 100000000) * preco_atual) / ordem["quantity"]
@@ -51,8 +47,7 @@ while True:
                 print(f"Ordem Fechada: Lucro no Bolso 🤑\nLucro Obtido: {lucro_liquido} 💰")
                 enviarMensagem(f"Ordem Fechada: Lucro no Bolso 🤑\nLucro Obtido: {lucro_liquido} 💰")
 
-        ################################ INJETANDO MARGEM NAS OPERAÇÕES ################################
-
+        # INJETANDO MARGEM NAS OPERAÇÕES
         protecao = 1.02
 
         for ordem in trades_abertos:
@@ -65,3 +60,4 @@ while True:
 
     except Exception as erro:
         print(f"❌ ERRO: {erro}")
+        enviarMensagem(f"❌ ERRO: {erro}")
--- _telegram.py
+++ _telegram.py
@@ -2,10 +2,10 @@ import telebot
 from decouple import config
 
 def enviarMensagem(mensagem):
-    bot = telebot.TeleBot(config("Token_Telegram"))
+    bot = telebot.TeleBot(str(config("TOKEN_TELEGRAM")))
 
     try:
-        bot.send_message(config("Id_Telegram"), f"{mensagem}")
+        bot.send_message(config("ID_TELEGRAM"), f"{mensagem}")
 
     except Exception as erro:
         print(f"Algo Errado Com o Telegram!")
