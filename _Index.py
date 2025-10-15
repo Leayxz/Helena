5c88e10caba348e4f1a6cb9fb2c928007fb30d18
Leandro Rodrigues <leandromartinsr10@hotmail.com>
2025-10-15 16:53:13 -0300

    .

--- _Index.py
+++ _Index.py
@@ -15,22 +15,22 @@ ultimo_preco = 110_619
 
 while True:
       try:
-            # ARMAZENAR ORDENS ABERTAS E MONTANTE DISPONIVEL
+            # BUSCA UMA VEZ ORDENS ABERTAS E MONTANTE ARMAZENAR ORDENS ABERTAS E MONTANTE DISPONIVEL
             user_data = json.loads(lnm.get_user())
             trades_abertos = json.loads(lnm.futures_get_trades({"type": "running"}))
+            print(f'Resposta User_data: {user_data}\n Resposta Trades_abertos: {trades_abertos}')
 
             # PREÇO ATUAL LNM OU PRECO ATUAL BYBIT
             try:
                   start = time.time()
-                  ticker = json.loads(lnm.futures_get_ticker())
-                  preco_atual = ticker["lastPrice"]
+                  preco_atual = requests.get("https://api.lnmarkets.com/v2/futures/ticker", params = {"symbol": "BTCUSD"}).json()['lastPrice']
                   print(f"🟦 Preço Atual Ln Markets: {preco_atual} | Tempo: {time.time() - start:.2f}")
-
+                  print(f"Saldo Atual: {user_data['balance']}")
             except:
                   url = "https://api.bybit.com/v5/market/tickers"
-                  params = {"category": "spot", "symbol": "BTCUSDT"}
+                  params = {"category": "spot", "symbol": "BTCUSDT"} # Spot??
                   ticker = requests.get(url, params = params).json()['result']['list'][0]
-                  preco_atual = ticker['lastPrice']
+                  preco_atual = float(ticker['lastPrice'])
                   print(f"🟧 Preço Atual Bybit: {preco_atual} | Tempo: {time.time() - start:.2f}")
 
             # ATUALIZA ATH
@@ -42,12 +42,12 @@ while True:
             preco_limite, variacao = 250_000, 0.007
             margin, leverage = 24400, 10
 
-            if user_data["balance"] > preco_limite and abs(preco_atual - ultimo_preco) >= ultimo_preco * variacao:
+            if user_data['balance'] > preco_limite and abs(preco_atual - ultimo_preco) >= ultimo_preco * variacao:
 
                   print("Variação Detectada. Enviando Ordem 🫡")
                   new_trade = json.loads(lnm.futures_new_trade({"type": "m", "side": "b", "margin": margin, "leverage": leverage}))
                   print(f"Ordem Enviada: Compra Efetivada ✅\nPreço De Compra: {new_trade['price']} 🎯")
-                  enviarMensagem(f"🔻 Distância ATH: {percentual_queda:.2f} \nOrdem Enviada: Compra Efetivada ✅\nPreço De Compra: {new_trade['price']} 🎯")
+                  enviarMensagem(f"🔻 Distância ATH: {percentual_queda:.2f}%\nOrdem Enviada: Compra Efetivada ✅\nPreço De Compra: {new_trade['price']} 🎯")
                   ultimo_preco = new_trade["price"]
 
             # FECHANDO ORDENS ABERTAS
